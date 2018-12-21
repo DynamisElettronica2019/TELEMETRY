8bd5459c35728acee4306626e94518d5f7e33c0d
Marco <marco.mandelli212@gmail.com>
2018-12-21 20:25:22 +0100

    Added string parsing
    Added string splitting part, and return of Parsed object

--- TelemetrySoftware2019/src/back_end/Parser.java
+++ TelemetrySoftware2019/src/back_end/Parser.java
@@ -10,12 +10,14 @@ public class Parser {
 	private char recogniserState;
 	private int lenDebug;
 	private char recogniserDebug;
+	private int currElement;
 
 	/*
 	 * Set class attributes through ConfReader
 	 */
 	public Parser() {
-			//TODO
+		// TODO
+		currElement = 0;
 	}
 
 	/*
@@ -24,25 +26,54 @@ public class Parser {
 	 */
 	Parsed parseString(String stringToParse) throws InvalidReadingException {
 		char recogniser;
+		Parsed parse = new Parsed();
 		recogniser = stringToParse.charAt(0);
 		if (recogniser == recogniserData) {
 			if (stringToParse.length() != lenData) {
 				throw new InvalidReadingException("Message lenght Error");
+			} else {
+				int startIndex = 0;
+				for (int i = 0; i < stringToParse.length(); i++) {
+					if (stringToParse.charAt(i) == ';') {
+						String strToAdd = stringToParse.substring(startIndex, i - 1);
+						startIndex = i + 1;
+						parse.parsedInfo[currElement] = strToAdd;
+						currElement++;
+					}
+				}
 			}
-		}
-		else if (recogniser == recogniserState) {
+		} else if (recogniser == recogniserState) {
 			if (stringToParse.length() != lenState) {
 				throw new InvalidReadingException("Message lenght Error");
+			} else {
+				int startIndex = 0;
+				for (int i = 0; i < stringToParse.length(); i++) {
+					if (stringToParse.charAt(i) == ';') {
+						String strToAdd = stringToParse.substring(startIndex, i - 1);
+						startIndex = i + 1;
+						parse.parsedInfo[currElement] = strToAdd;
+						currElement++;
+					}
+				}
+
 			}
-		}
-		else if (recogniser == recogniserDebug) {
+		} else if (recogniser == recogniserDebug) {
 			if (stringToParse.length() != lenDebug) {
 				throw new InvalidReadingException("Message lenght Error");
+			} else {
+				int startIndex = 0;
+				for (int i = 0; i < stringToParse.length(); i++) {
+					if (stringToParse.charAt(i) == ';') {
+						String strToAdd = stringToParse.substring(startIndex, i - 1);
+						startIndex = i + 1;
+						parse.parsedInfo[currElement] = strToAdd;
+						currElement++;
+					}
+				}
 			}
-		}
-		else {
+		} else {
 			throw new InvalidReadingException("First letter reading Error");
 		}
+		return parse;
 	}
-
 }
