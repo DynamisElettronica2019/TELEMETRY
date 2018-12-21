3853dab3701b8bf458b56c48e93eea7b24554583
Marco <marco.mandelli212@gmail.com>
2018-12-21 18:49:37 +0100

    Useless Commit
    ------------

--- TelemetrySoftware2019/src/back_end/Parser.java
+++ TelemetrySoftware2019/src/back_end/Parser.java
@@ -3,27 +3,40 @@ package back_end;
 import exceptions.InvalidReadingException;
 
 public class Parser {
-	
+
 	private int lenData;
 	private char recogniserData;
 	private int lenState;
 	private char recogniserState;
 	private int lenDebug;
 	private char recogniserDebug;
-	
+
 	/*
 	 * Set class attributes through ConfReader
 	 */
-	public Parser() {
-		// TODO
+	public Parser(String receivedString) {
+			char recogniser;
+			recogniser = receivedString.charAt(0);
+			if (recogniser == recogniserData) {
+				
+			}
+			else if (recogniser == recogniserState) {
+				
+			}
+			else if (recogniser == recogniserDebug) {
+				
+			}
+			else {
+				
+			}
 	}
-	
+
 	/*
-	 * Check input string validity and create corresponding Parsed object.
-	 * Throw exception if the input string is not valid.
+	 * Check input string validity and create corresponding Parsed object. Throw
+	 * exception if the input string is not valid.
 	 */
 	Parsed parseString(String stringToParse) throws InvalidReadingException {
-		//TODO
+		// TODO
 	}
-	
+
 }
