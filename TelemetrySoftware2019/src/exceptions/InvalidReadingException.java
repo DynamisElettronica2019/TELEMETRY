2368953423fd99006f35452ec3c7a47390ad895b
Marco <marco.mandelli212@gmail.com>
2018-12-21 19:07:46 +0100

    Added exception constructor

--- TelemetrySoftware2019/src/exceptions/InvalidReadingException.java
+++ TelemetrySoftware2019/src/exceptions/InvalidReadingException.java
@@ -1,14 +1,19 @@
 package exceptions;
 
 public class InvalidReadingException extends Exception {
-
 	private static final long serialVersionUID = 7802809916810716256L;
 
 	/*
 	 * Print exception timestamp and error type to console
 	 */
-	void log(String type) {
+	
+	public InvalidReadingException(String Errore) {
+		log(Errore);
+	}
+	
+	private void log(String type) {
 		//TODO
 	}
-
+	
+	
 }
