5ddbeac0ecb4e7e9146ab52d4a5fa1bcf38fb0eb
Marco <marco.mandelli212@gmail.com>
2018-12-21 20:38:02 +0100

    Parsed first commit
    Created parsedInfo set function

--- TelemetrySoftware2019/src/back_end/Parsed.java
+++ TelemetrySoftware2019/src/back_end/Parsed.java
@@ -3,5 +3,15 @@ package back_end;
 public class Parsed {
 	
 	private String[] parsedInfo;
+	private int currElement;
 	
+	public Parsed() {
+		currElement = 0;
+	}
+	
+	public void parsedEdit(String strToAdd)
+	{
+		parsedInfo[currElement] = strToAdd;
+		currElement++;
+	}
 }
