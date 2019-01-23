a8ce5477217b69745802032a2b9e50cdb312fc52
Unknown <gioo27_10@tiscali.it>
2019-01-23 16:51:53 +0100

    Conf File Update

--- TelemetrySoftware2019/src/exceptions/InvalidReadingException.java
+++ TelemetrySoftware2019/src/exceptions/InvalidReadingException.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class InvalidReadingException extends MyException {
 	private static final long serialVersionUID = 1L;
-	private static final String type = "Reading error";
+	private static final String type = "[Reading error]";
 	
 	public InvalidReadingException(String err) {
 		super(err);
