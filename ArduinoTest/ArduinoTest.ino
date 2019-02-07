dbfee2b38431fb29b3c9deaf4649474cca67b3f3
Unknown <marco.mandelli212@gmail.com>
2019-02-07 22:28:13 +0100

    Ack miss
    added 1 every 10 ack miss

--- ArduinoTest/ArduinoTest.ino
+++ ArduinoTest/ArduinoTest.ino
@@ -4,6 +4,7 @@
 Timer t;
 bool received, started;
 int state = 0;
+int randomState = 0;
 int pos = 0;
 char dataToSend = "A0000000;000.0;000.0;000.0;000.0;000;000;000;000;000;000;000;00.0;0;00000;000;000;000.0;000.0;0000.0;0;0;000;0;00000;00000;0.000;00000;0;0;000;000;00.00;00000;00.0;00.00;00000;00.0;00000;00.00;00000;00.00;00000;00000;00000;000;000;000;000;000;000;000;000;000;000;000;000;0000.00;0000.00;000.00;0000.00;0000.00;0000.00;000;000;0;0;0;000;000;00000;00000;00000;00000;00000;00000;000;0000;000;0000;000;0000;000;0000;000;0000000000;0000000;0000000;0000000;000000000000000000000000000000000000000000000000000000000000";
 char debugToSend = "X000;0000;000;0000;000;0000;000;0000;000;000;0000;0000;000;00000;00000;00000;00000;00000;000000";
@@ -24,37 +25,49 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(received=true) {
+  if((received==true) && (randomState<9)) {
     switch(strReceived[1])
     {
       case 'M':
         Serial.write("[AM]");
+        randomState++;
         break;
       case 'N':
         Serial.write("[AN]");
+        randomState++;
         break;
       case 'B':
         Serial.write("[AB]");
+        randomState++;
         break; 
       case 'V':
         Serial.write("[AV]");
+        randomState++;
         break;
       case 'X':
         Serial.write("[AX]");
+        randomState++;
         break;
       case 'Z':
         Serial.write("[AZ]");
+        randomState++;
         break;
       case 'J':
         Serial.write("[AJ]");
+        randomState++;
         break;
       case 'H':
         Serial.write("[AH]");
+        randomState++;
         break;
       case 'R':
         Serial.write("[AR11:22:33;01:01:2019]");
+        randomState++;
         break;
     }
+  }
+  else if(randomState == 10) {
+    randomState = 0;
   }
 }
 
