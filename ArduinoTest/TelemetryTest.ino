54a1ac965f33d17526cac1a2247661deae69dd5a
Unknown <marco.mandelli212@gmail.com>
2019-02-07 21:22:15 +0100

    First commit
    Telemetry test init

---
+++ ArduinoTest/TelemetryTest.ino
@@ -0,0 +1,92 @@
+#include <Event.h>
+#include <Timer.h>
+
+Timer t;
+bool received, started;
+int state = 0;
+int pos = 0;
+char dataToSend = "A0000000;000.0;000.0;000.0;000.0;000;000;000;000;000;000;000;00.0;0;00000;000;000;000.0;000.0;0000.0;0;0;000;0;00000;00000;0.000;00000;0;0;000;000;00.00;00000;00.0;00.00;00000;00.0;00000;00.00;00000;00.00;00000;00000;00000;000;000;000;000;000;000;000;000;000;000;000;000;0000.00;0000.00;000.00;0000.00;0000.00;0000.00;000;000;0;0;0;000;000;00000;00000;00000;00000;00000;00000;000;0000;000;0000;000;0000;000;0000;000;0000000000;0000000;0000000;0000000;000000000000000000000000000000000000000000000000000000000000";
+char debugToSend = "X000;0000;000;0000;000;0000;000;0000;000;000;0000;0000;000;00000;00000;00000;00000;00000;000000";
+char stateToSend = "C0000000";
+char strReceived[20];
+
+void SendData();
+void SendDebug();
+void SendState();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  t.every(500, SendData, 0);
+  t.every(2000, SendDebug, 0);
+  t.every(10000, SendState, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(received=true) {
+    switch(strReceived[1])
+    {
+      case 'M':
+        Serial.write("[AM]");
+        break;
+      case 'N':
+        Serial.write("[AN]");
+        break;
+      case 'B':
+        Serial.write("[AB]");
+        break; 
+      case 'V':
+        Serial.write("[AV]");
+        break;
+      case 'X':
+        Serial.write("[AX]");
+        break;
+      case 'Z':
+        Serial.write("[AZ]");
+        break;
+      case 'J':
+        Serial.write("[AJ]");
+        break;
+      case 'H':
+        Serial.write("[AH]");
+        break;
+      case 'R':
+        Serial.write("[AR11:22:33;01:01:2019]");
+        break;
+    }
+  }
+}
+
+void SendData()
+{
+  Serial.write(dataToSend);
+}
+
+void SendDebug()
+{
+  Serial.write(debugToSend);
+}
+
+void SendState()
+{
+  Serial.write(stateToSend);
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char recChar = (char)Serial.read();
+    if (recChar == '[') {
+      started = true;
+      received = false;
+      pos = 0;
+    }
+    else if (recChar == ']') {
+      received = true;
+    }
+    else {
+      strReceived[pos] = recChar;
+      pos++;
+    }
+  }
+}
