af227fa308283709f20dcebbd49caea72e6239b6
Unknown <marco.mandelli212@gmail.com>
2019-02-08 13:49:46 +0100

    Debug message variation

--- ArduinoTest/ArduinoTest.ino
+++ ArduinoTest/ArduinoTest.ino
@@ -8,6 +8,8 @@
 Timer t;
 bool received, started;
 int state = 2;
+int debugCount = 1;
+int secondDebug = 0;
 int randomState = 0;
 int pos = 0;
 char dataToSend[] = "A;0000000;000.0;000.0;000.0;000.0;000;000;000;000;000;000;000;00.0;0;00000;000;000;000.0;000.0;0000.0;0;0;000;0;00000;00000;0.000;00000;0;0;000;000;00.00;00000;00.0;00.00;00000;00.0;00000;00.00;00000;00.00;00000;00000;00000;000;000;000;000;000;000;000;000;000;000;000;000;0000.00;0000.00;000.00;0000.00;0000.00;0000.00;000;000;0;0;0;000;000;00000;00000;00000;00000;00000;00000;000;0000;000;0000;000;0000;000;0000;000;0000000000;0000000;0000000;0000000;00000000000000000000000000000000000000000000000000000000000";
@@ -83,6 +85,55 @@ void SendData()
 void SendDebug()
 {
   Serial.write(debugToSend);
+  if((debugCount == 9) && (secondDebug == 0)) {
+    secondDebug = 1;
+    debugCount = 0;
+  }
+  else if ((debugCount == 9) && (secondDebug == 1)) {
+    debugToSend[4-secondDebug] = 0;
+    debugToSend[9-secondDebug] = 0;
+    debugToSend[13-secondDebug] = 0;
+    debugToSend[18-secondDebug] = 0;
+    debugToSend[22-secondDebug] = 0;
+    debugToSend[27-secondDebug] = 0;
+    debugToSend[31-secondDebug] = 0;
+    debugToSend[36-secondDebug] = 0;
+    debugToSend[40-secondDebug] = 0;
+    debugToSend[44-secondDebug] = 0;
+    debugToSend[49-secondDebug] = 0;
+    debugToSend[54-secondDebug] = 0;
+    debugToSend[58-secondDebug] = 0;
+    debugToSend[64-secondDebug] = 0;
+    debugToSend[70-secondDebug] = 0;
+    debugToSend[76-secondDebug] = 0;
+    debugToSend[82-secondDebug] = 0;
+    debugToSend[88-secondDebug] = 0;
+    debugToSend[95-secondDebug] = 0;
+    secondDebug = 0;
+    debugCount = 1;
+  }
+  else {
+    debugCount++;
+    debugToSend[4-secondDebug] = debugCount;
+    debugToSend[9-secondDebug] = debugCount;
+    debugToSend[13-secondDebug] = debugCount;
+    debugToSend[18-secondDebug] = debugCount;
+    debugToSend[22-secondDebug] = debugCount;
+    debugToSend[27-secondDebug] = debugCount;
+    debugToSend[31-secondDebug] = debugCount;
+    debugToSend[36-secondDebug] = debugCount;
+    debugToSend[40-secondDebug] = debugCount;
+    debugToSend[44-secondDebug] = debugCount;
+    debugToSend[49-secondDebug] = debugCount;
+    debugToSend[54-secondDebug] = debugCount;
+    debugToSend[58-secondDebug] = debugCount;
+    debugToSend[64-secondDebug] = debugCount;
+    debugToSend[70-secondDebug] = debugCount;
+    debugToSend[76-secondDebug] = debugCount;
+    debugToSend[82-secondDebug] = debugCount;
+    debugToSend[88-secondDebug] = debugCount;
+    debugToSend[95-secondDebug] = debugCount;
+  }
 }
 
 void SendState()
