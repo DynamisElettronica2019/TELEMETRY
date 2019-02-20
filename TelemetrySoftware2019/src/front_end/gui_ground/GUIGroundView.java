85a8cf636daceda32bdd05e22fdac0503528e6ef
Unknown <marco.mandelli212@gmail.com>
2019-02-20 14:36:47 +0100

    FXML

--- TelemetrySoftware2019/src/front_end/gui_ground/GUIGroundView.java
+++ TelemetrySoftware2019/src/front_end/gui_ground/GUIGroundView.java
@@ -1,12 +1,17 @@
 package front_end.gui_ground;
 
+import java.io.IOException;
+
 import back_end.Channel;
 import back_end.Command;
 import back_end.Debug;
 import back_end.Error;
 import back_end.State;
 import front_end.View;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -17,16 +22,19 @@ public class GUIGroundView extends View {
 	
 	private Stage stageSX;
 	private Stage stageDX;
+	private Parent firstPaneSX;
+	private Parent firstPaneDX;
 	
-	public GUIGroundView() {
+	public GUIGroundView() throws IOException {
 		System.out.println("Starting GUI view (Ground Station Version)..");
 		setDoubleScreen();
 		
 		//TODO
 	}
 	
-	private void setDoubleScreen(){
+	private void setDoubleScreen() throws IOException{
 		//Set stage SX
+		firstPaneSX = FXMLLoader.load(getClass().getResource("SxStage.fxml"));
 		stageSX = new Stage();
 		stageSX.setTitle("Telemetry2019_SX");
 		Screen mainScreen = Screen.getScreens().get(0); 
@@ -35,8 +43,10 @@ public class GUIGroundView extends View {
         stageSX.setY(bounds.getMinY()+SCREEN_OFFSET_Y);
         stageSX.setMaximized(true);
         stageSX.setOnCloseRequest( event -> {stageDX.close();} );
+        stageSX.setScene(new Scene(firstPaneSX));
         stageSX.show();
         //Set stage DX
+        firstPaneDX = FXMLLoader.load(getClass().getResource("DxStage.fxml"));
         stageDX = new Stage();
         stageDX.setTitle("Telemetry2019_DX");
         Screen secondScreen = Screen.getScreens().get(0);
@@ -46,6 +56,7 @@ public class GUIGroundView extends View {
         stageDX.setY(bounds.getMinY()+SCREEN_OFFSET_Y);
         stageDX.setMaximized(true);
         stageDX.setOnCloseRequest( event -> {stageSX.close();} );
+        stageSX.setScene(new Scene(firstPaneDX));
         stageDX.show();
 	}
 
---
+++ TelemetrySoftware2019/src/front_end/gui_ground/SxController.java
@@ -0,0 +1,5 @@
+package front_end.gui_ground;
+
+public class SxController {
+
+}
