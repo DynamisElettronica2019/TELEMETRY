675c1d27ad63b57f12bcf74fa0537e1b3417ea8b
Unknown <marco.mandelli212@gmail.com>
2019-03-03 11:23:56 +0100

    Added threshold indicators

--- TelemetrySoftware2019/src/front_end/gui_ground/TopBarController.java
+++ TelemetrySoftware2019/src/front_end/gui_ground/TopBarController.java
@@ -12,6 +12,7 @@ import back_end.Command;
 import back_end.Debug;
 import back_end.State;
 import back_end.Threshold;
+import back_end.ThresholdChannel;
 import back_end.Error;
 import back_end.LapTimer;
 import back_end.ViewLoader;
@@ -34,7 +35,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 public class TopBarController extends Controller {
-	private static final int STATES = 11;
+	private static final int STATES = 6;
 	private static final String MAP_CHANNEL = "GCU_AUTOX_FB";
 	private static final String TRACTION_CHANNEL = "GCU_TRACTION_FB";
 	private Map<String, Circle> stateMap = new HashMap<>();
@@ -70,32 +71,22 @@ public class TopBarController extends Controller {
 	@Override
 	public void SetState() { 
 		stateList = ConfReader.getNames("states");
-		circleList = new Circle[STATES];
-		labelList = new Label[STATES];
+		circleList = new Circle[11];
+		labelList = new Label[11];
 		circleList[0] = circle1;
 		circleList[1] = circle2;
 		circleList[2] = circle3;
 		circleList[3] = circle4;
 		circleList[4] = circle5;
 		circleList[5] = circle6;
-		circleList[6] = circle7;
-		circleList[7] = circle8;
-		circleList[8] = circle9;
-		circleList[9] = circle10;
-		circleList[10] = circle11;
 		labelList[0] = label1;
 		labelList[1] = label2;
 		labelList[2] = label3;
 		labelList[3] = label4;
 		labelList[4] = label5;
 		labelList[5] = label6;
-		labelList[6] = label7;
-		labelList[7] = label8;
-		labelList[8] = label9;
-		labelList[9] = label10;
-		labelList[10] = label11;
 				
-		for(int i=0; i<7; i++) {
+		for(int i=0; i<STATES; i++) {
 			stateMap.put(stateList.get(i), circleList[i]);
 			labelList[i].setText(stateList.get(i));
 		}
@@ -185,16 +176,42 @@ public class TopBarController extends Controller {
 		});
 	}
 
+	//Set labels, you cannot have more than 5 Threshold states
 	@Override
 	public void SetTS() {
-		// TODO Auto-generated method stub
-		
+		int element = STATES+1;
+		ArrayList<String> chNames = ConfReader.getNames("channels");
+		circleList[6] = circle7;
+		circleList[7] = circle8;
+		circleList[8] = circle9;
+		circleList[9] = circle10;
+		circleList[10] = circle11;
+		labelList[6] = label7;
+		labelList[7] = label7;
+		labelList[8] = label8;
+		labelList[9] = label9;
+		labelList[10] = label10;
+		for(int i=0;i<chNames.size();i++) {
+			String[] NaTh = ConfReader.haveThreshold(chNames.get(i));
+			if (NaTh.length>1) {
+				stateMap.put(NaTh[0], circleList[element-1]);
+				labelList[element].setText(chNames.get(i));
+				element++;
+			}
+			if (element > 11) {
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void EditTS(Threshold thresholdState) {
-		// TODO Auto-generated method stub
-		
+		if (thresholdState.isError()) {
+			stateMap.get(thresholdState.getChName()).setFill(Color.RED);
+		}
+		else {
+			stateMap.get(thresholdState.getChName()).setFill(Color.GREEN);
+		}
 	}
 	
 	/*
