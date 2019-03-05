883aaa19c59a950ac7841867134128c782afd53d
Unknown <marco.mandelli212@gmail.com>
2019-03-05 20:06:06 +0100

    4 grafici

--- TelemetrySoftware2019/src/front_end/gui_ground/EngineScreenController.java
+++ TelemetrySoftware2019/src/front_end/gui_ground/EngineScreenController.java
@@ -29,17 +29,20 @@ public class EngineScreenController extends Controller {
 	private final String OUT_OIL_TEMP = "tOil_Out";
 	private final String IN_WATER_L_TEMP = "tWaterL_In";
 	private final String OUT_WATER_L_TEMP = "tWaterL_Out";
+	private final String EXHAUST_TEMP_1 = "tExhaust_1";
+	private final String EXHAUST_TEMP_2 = "tExhaust_2";
+	private final String OIL_PRESS = "pOil";
 	private final String LAST_TIME = "time";
 	private Series<String, Double> oilTempIn, oilTempOut;
 	private Series<String, Double> waterTempLIN, waterTempLOut;
-	private ObservableList<XYChart.Series<String,Double>> waterTempChartData, oiltempChartData;
+	private Series<String, Double> exhaust1Temp, exhaust2Temp;
+	private Series<String, Double> oilPress;
+	private ObservableList<XYChart.Series<String,Double>> waterTempChartData, oiltempChartData, exhaustTempChartData, pressChartData;
 	private int elCount = 0;
 	@FXML
-	private LineChart<String, Double> oilTempChart, waterTempChart;
+	private LineChart<String, Double> oilTempChart, waterTempChart, exhaustTempChart, pressChart;
 	@FXML
-	private Label oilTempInLabel, oilTempOutLabel, waterTempInLLabel, waterTempOutLLabel;
-	@FXML
-	private TableView table;
+	private Label oilTempInLabel, oilTempOutLabel, waterTempInLLabel, waterTempOutLLabel, exhaust1TempLabel, exhaust2TempLabel, oilPressLabel;
 	
 	@Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -141,6 +144,33 @@ public class EngineScreenController extends Controller {
 			    }
 			});
 		}
+		else if(channel.getName().equals(EXHAUST_TEMP_1)) {
+			Platform.runLater(new Runnable() {
+			    @Override
+			    public void run() {
+			    	exhaust1Temp.getData().add(channel.getLastChartEl());
+			    	exhaust1TempLabel.setText(Double.toString(channel.getLastElems(1).get(0)));
+			    }
+			});
+		}
+		else if(channel.getName().equals(EXHAUST_TEMP_2)) {
+			Platform.runLater(new Runnable() {
+			    @Override
+			    public void run() {
+			    	exhaust2Temp.getData().add(channel.getLastChartEl());
+			    	exhaust2TempLabel.setText(Double.toString(channel.getLastElems(1).get(0)));
+			    }
+			});
+		}
+		else if(channel.getName().equals(OIL_PRESS)) {
+			Platform.runLater(new Runnable() {
+			    @Override
+			    public void run() {
+			    	oilPress.getData().add(channel.getLastChartEl());
+			    	oilPressLabel.setText(Double.toString(channel.getLastElems(1).get(0)));
+			    }
+			});
+		}
 	}
 
 	@Override
@@ -194,19 +224,36 @@ public class EngineScreenController extends Controller {
 	private void SetChart() {
 		waterTempChartData = FXCollections.observableArrayList();
 		oiltempChartData = FXCollections.observableArrayList();
+		exhaustTempChartData = FXCollections.observableArrayList();
+		pressChartData = FXCollections.observableArrayList();
+		
 		oilTempIn = new Series<>();
 		oilTempOut = new Series<>();
 		waterTempLIN = new Series<>();
 		waterTempLOut = new Series<>();
+		exhaust1Temp = new Series<>();
+		exhaust2Temp = new Series<>();
+		oilPress = new Series<>();
+		
 		oilTempIn.setName("Oil Temp In");
 		oilTempOut.setName("Oil Temp Out");
 		waterTempLIN.setName("Water temp left In");
 		waterTempLOut.setName("Water temp left Out");
+		exhaust1Temp.setName("Exhaust 1 Temp");
+		exhaust2Temp.setName("Exhaust 2 Temp");
+		oilPress.setName("Oil Pression");
+		
 		waterTempChartData.add(waterTempLIN);
 		waterTempChartData.add(waterTempLOut);
 		oiltempChartData.add(oilTempIn);
 		oiltempChartData.add(oilTempOut);
+		exhaustTempChartData.add(exhaust1Temp);
+		exhaustTempChartData.add(exhaust2Temp);
+		pressChartData.add(oilPress);
+		
 		waterTempChart.setData(waterTempChartData);
 		oilTempChart.setData(oiltempChartData);
+		exhaustTempChart.setData(exhaustTempChartData);
+		pressChart.setData(pressChartData);
 	}
 }
