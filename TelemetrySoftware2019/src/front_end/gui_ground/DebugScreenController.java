37c435dce4a0cca67c0c1864c6be7b862e01d668
Unknown <gioo27_10@tiscali.it>
2019-05-27 13:19:01 +0200

    Delete warnings + debug

--- TelemetrySoftware2019/src/front_end/gui_ground/DebugScreenController.java
+++ TelemetrySoftware2019/src/front_end/gui_ground/DebugScreenController.java
@@ -1,5 +1,10 @@
 package front_end.gui_ground;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
 import back_end.Channel;
 import back_end.Command;
 import back_end.Debug;
@@ -7,13 +12,36 @@ import back_end.Error;
 import back_end.LapTimer;
 import back_end.State;
 import back_end.Threshold;
+import eu.hansolo.medusa.Gauge;
+import javafx.fxml.FXML;
 
 public class DebugScreenController extends Controller {
+	
+	private Map<String, Gauge> stateMap = new HashMap<>();
+	
+	@FXML
+	private Gauge dcuTemp, dcuCurr, dauFLTemp, dauFLCurr, dauFRTemp, dauFRCurr, dauRearTemp, dauRearCurr;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		super.initialize(location, resources);
+		initMap();
+	}
+
+	private void initMap() {
+		stateMap.put("DCU_TEMP", dcuTemp);
+		stateMap.put("DCU_CURRENT", dcuCurr);
+		stateMap.put("DAU_FL_TEMP", dauFLTemp);
+		stateMap.put("DAU_FL_CURRENT", dauFLCurr);
+		stateMap.put("DAU_FR_TEMP", dauFRTemp);
+		stateMap.put("DAU_FR_CURRENT", dauFRCurr);
+		stateMap.put("DAU_REAR_TEMP", dauRearTemp);
+		stateMap.put("DAU_REAR_CURRENT", dauRearCurr);
+	}
 
 	@Override
 	public void editDebug(Debug debug) {
-		// TODO Auto-generated method stub
-		
+		if(stateMap.get(debug.getName()) != null) stateMap.get(debug.getName()).setValue(debug.getValue());
 	}
 
 	@Override
@@ -24,10 +52,7 @@ public class DebugScreenController extends Controller {
 	
 	@Override
 	public void editChannel(Channel channel) {
-		if(!channel.isEmpty()){
-			// TODO Auto-generated method stub
-			
-		}
+		// TODO Auto-generated method stub
 	}
 
 	@Override
