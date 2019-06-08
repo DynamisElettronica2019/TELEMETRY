f6961cdbfcfd3de6b73b9cad2f90f437686aa0b7
Unknown <marco.mandelli212@gmail.com>
2019-06-08 12:00:05 +0200

    Command screen flowpanel and buttons css

--- TelemetrySoftware2019/src/front_end/gui_ground/CommandScreenController.java
+++ TelemetrySoftware2019/src/front_end/gui_ground/CommandScreenController.java
@@ -8,37 +8,28 @@ import java.util.ResourceBundle;
 
 import back_end.Channel;
 import back_end.Command;
-import back_end.CommandSender;
 import back_end.Debug;
 import back_end.Error;
 import back_end.LapTimer;
 import back_end.State;
 import back_end.Threshold;
 import configuration.ConfReader;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ToolBar;
+import javafx.scene.layout.FlowPane;
 
 public class CommandScreenController extends Controller {
-	private Map<String, String> driverMessageMap = new HashMap<>();
 	private Map<Button, String> buttonMap = new HashMap<>();
 	private Map<String, Button> buttonMapReverse = new HashMap<>();
-	ObservableList<String> driverMessages;
 	ArrayList<String> nameButtonList;
 	ArrayList<Button> commandButtons;
 	@FXML
-	private ToolBar buttonBar;
-	@FXML
-	private Button StartAcqButton, StopAcqButton, StartTelemButton, StopTelemButton;
+	private FlowPane buttonBar;
 	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		super.initialize(location, resources);
-		driverMessages = FXCollections.observableArrayList("Stop the car", "Keep going", "Pit");
 		
 		//Set all the buttons from config file
 		nameButtonList = new ArrayList<String>();
@@ -47,9 +38,10 @@ public class CommandScreenController extends Controller {
 		
 		for(int i=0; i < nameButtonList.size(); i++) {
 			commandButtons.add(new Button(nameButtonList.get(i)));
+			commandButtons.get(i).getStyleClass().add("command-buttons");
 			buttonMap.put(commandButtons.get(i), nameButtonList.get(i));
 			buttonMapReverse.put(nameButtonList.get(i), commandButtons.get(i));
-			buttonBar.getItems().add(commandButtons.get(i));
+			buttonBar.getChildren().add(commandButtons.get(i));
 		}
 		
 		for(Button but : commandButtons) {
@@ -58,10 +50,6 @@ public class CommandScreenController extends Controller {
 				but.setDisable(true);
 			});
 		}
-
-		driverMessageMap.put("Stop the car", "CAR_STOP");
-		driverMessageMap.put("Keep going", "GO");
-		driverMessageMap.put("Pit", "PIT");
 	}
 	
 	@Override
